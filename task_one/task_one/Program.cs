f5cc4c023dbc231888fd71098627bfc8c665a183
bormannVadim <freedom-mv47@rambler.ru>
2019-06-21 21:37:45 +0300

    Создал форму

---
+++ task_one/task_one/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace task_one
+{
+    class Program:Form
+    {
+        public static void Main()
+        {
+            Program pr = new Program();
+            pr.Show();
+        }
+
+        public Program()
+        {
+
+            Text = "Привет";
+        }
+    }
+}
