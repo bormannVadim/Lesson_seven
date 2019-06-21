fefbbb1d12b6d5b38e25af16d25b9ba0df2dad7e
bormannVadim <freedom-mv47@rambler.ru>
2019-06-21 22:18:30 +0300

    Сделал первое задание

--- task_one/Task_1/Form1.Designer.cs
+++ task_one/Task_1/Form1.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Play = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "x2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Сброс";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 34);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Статистика:\nУдвоитель: 0";
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(174, 177);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(100, 44);
+            this.Play.TabIndex = 5;
+            this.Play.Text = "Играть";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 34);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Давай поиграем...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(293, 257);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Домашнее задание";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- task_one/Task_1/Form1.cs
+++ task_one/Task_1/Form1.cs
@@ -14,7 +14,83 @@ namespace Task_1
     {
         public Form1()
         {
+            // Савенко Вадим 
             InitializeComponent();
+
+        }
+        private int Udvoitel;
+        private int PlayNumber;
+        private int attemps;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = (int.Parse(label1.Text) + 1).ToString();
+            attemps++;
+            CheckIfWin(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = (int.Parse(label1.Text) * 2).ToString();
+            Udvoitel++;
+            label2.Text = "Статистика:\nУдвоитель: " + Udvoitel;
+            attemps++;
+            CheckIfWin();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "1";
+            Udvoitel = 0;
+            label2.Text = "Статистика:\nУдвоитель: "+ Udvoitel;
+            label3.Text = "Давай поиграем...";
+            attemps = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            Random rn = new Random();
+            PlayNumber = rn.Next(1, 100);
+            label3.Text = "Вам нужно\nполучить число: " + PlayNumber;
+        }
+
+        private void CheckIfWin()
+        {
+            int currentValue = Convert.ToInt32(label1.Text);
+            int targetValue = PlayNumber;
+
+            if (currentValue == PlayNumber)
+                //зупуск логики и подсчёт, выиграл или нет;
+                if (attemps == maxAttempts())
+                    MessageBox.Show("Поздравляем, вы выиграли!\nКол-во ходов: " + attemps);
+                else
+                    MessageBox.Show("Вы проиграли!\nКол-во ходов: " + attemps + "\nМакс. кол-во ходов: " + maxAttempts());
+            else if (currentValue >= PlayNumber)
+                MessageBox.Show("Вы проиграли!\nКол-во ходов: " + attemps + "\nНе попали в число");
+        }
+
+        private int maxAttempts()
+        {
+            int currentNumber = PlayNumber;
+            int MaxAttempts = 0;
+            while(currentNumber!=1)
+            {
+                if ((currentNumber % 2) == 0)
+                    currentNumber /= 2;
+                else
+                {
+                    currentNumber -= 1;
+                }
+                MaxAttempts++;
+            }
+
+            return MaxAttempts;
         }
     }
 }
